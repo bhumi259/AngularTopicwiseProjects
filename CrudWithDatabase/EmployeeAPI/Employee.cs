71bb25bb50d531882021ff9f9f8ebdc421108778
bhumi259 <114463220+bhumi259@users.noreply.github.com>
2022-11-02 14:49:57 +0530

    Add files via upload

---
+++ CrudWithDatabase/EmployeeAPI/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeAPI
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }=string.Empty;
+        public string Gender { get; set; } = string.Empty;
+        public string Salary { get; set; } = string.Empty;
+    }
+}
